669040f5817ac728b7653678224f70af1cfdca2d
dnlKaizer <danilokaizer@gmail.com>
2025-07-23 19:06:10 -0300

    add: controle de acesso dos botões da home

--- APP/src/app/components/home-page/home-page.component.ts
+++ APP/src/app/components/home-page/home-page.component.ts
@@ -10,11 +10,18 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  isAdmin: boolean = false;
+  isProfessor: boolean = false;
+  isAluno: boolean = false;
 
   constructor(
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) {
+    this.isAdmin = authService.isAdmin();
+    this.isProfessor = authService.isProfessor();
+    this.isAluno = authService.isAluno();
+  }
 
   /**
    * Executa o logout do usuário e redireciona para a página de login
@@ -23,4 +30,37 @@ export class HomePageComponent {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
+
+  goToAlunosPage() {
+    this.router.navigate([''])
+  }
+
+  goToProfessoresPage() {
+    this.router.navigate([''])
+  }
+
+  goToTurmasPage() {
+    this.router.navigate([''])
+  }
+
+  goToDisciplinasPage() {
+    this.router.navigate([''])
+  }
+
+  goToNotasPage() {
+    this.router.navigate([''])
+  }
+
+  goToAvaliacoesPage() {
+    this.router.navigate([''])
+  }
+
+  goToMatriculasPage() {
+    this.router.navigate([''])
+  }
+
+  goToDesempenhoPage() {
+    this.router.navigate([''])
+  }
+
 }
