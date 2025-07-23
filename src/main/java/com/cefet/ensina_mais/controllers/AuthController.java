4ca7395517049b3f3af5a9777d24234b477852a3
dnlKaizer <danilokaizer@gmail.com>
2025-07-22 22:23:09 -0300

    fix: correção do import errado de @RequestBody

--- src/main/java/com/cefet/ensina_mais/controllers/AuthController.java
+++ src/main/java/com/cefet/ensina_mais/controllers/AuthController.java
@@ -7,14 +7,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cefet.ensina_mais.dto.JwtAuthenticationDTO;
 import com.cefet.ensina_mais.dto.LoginDTO;
 import com.cefet.ensina_mais.security.JwtTokenProvider;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/auth")
--- src/main/java/com/cefet/ensina_mais/controllers/UsuarioController.java
+++ src/main/java/com/cefet/ensina_mais/controllers/UsuarioController.java
@@ -6,13 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cefet.ensina_mais.entities.Usuario;
 import com.cefet.ensina_mais.services.UsuarioService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/usuarios")
