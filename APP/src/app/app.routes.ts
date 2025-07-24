713498d92ded3eb10303161a417407215ccdafc0
dnlKaizer <danilokaizer@gmail.com>
2025-07-24 10:53:46 -0300

    add: página de matrículas

--- APP/src/app/app.routes.ts
+++ APP/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProfessoresPageComponent } from './components/professores-page/professores-page.component';
 import { DisciplinasPageComponent } from './components/disciplinas-page/disciplinas-page.component';
+import { MatriculasPageComponent } from './components/matriculas-page/matriculas-page.component';
 
 export const routes: Routes = [
     // Rota padrão - redireciona para login
@@ -20,6 +21,9 @@ export const routes: Routes = [
 
     // Rota para a página disciplinas
     { path: 'disciplinas', component: DisciplinasPageComponent },
+
+    // Rota para a página matrículas
+    { path: 'matriculas', component: MatriculasPageComponent },
 
     // Rota para páginas não encontradas
     { path: '**', redirectTo: '/login' }
